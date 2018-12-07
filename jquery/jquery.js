6718b3b3e961a5a1c5177278034586b0b13d15f2
Zach Feldman <maisaimamovic@gmail.com>
2018-12-07 11:30:46 +0100

    changes

---
+++ jquery/jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  
+$(".one").fadeOut(5000).fadeIn(900);
+$(".two").fadeIn(1000).fadeOut(1200).fadeIn(1000);
+$(".three").slideUp(2000).slideDown(2000);
+$(".four").slideUp(2000).slideDown(3000);
+$(".five").slideUp(5000).slideDown(5000);
+$(".six").slideUp(3000).slideDown(3000);
+$(".seven").slideToggle(500).fadeIn(700);
+$(".eight").fadeOut(5000).fadeIn(5000)
+
+    
+$(".run").animate({left: '250px'});
+    
+});
+
